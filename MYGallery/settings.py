ee3431dba928d29a3b6733656bf180d3c5f9aadc
Your Name <you@example.com>
2021-09-06 07:48:18 +0300

    my gallery

--- MYGallery/settings.py
+++ MYGallery/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+# See https://docs.djangmanage.py check --deployoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'hduxlg*r#tev!kh0uh0ss6@++nlx18+*=-hz#t1l@#&+&)5o=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn MYGallery.wsgi --log-file -
+web: gunicorn manage:app
