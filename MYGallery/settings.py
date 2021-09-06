06298073d4d3cc3e9d442bbac2ccbe17685cac57
Your Name <you@example.com>
2021-09-06 09:26:17 +0300

    make it better

--- MYGallery/settings.py
+++ MYGallery/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import django_heroku
-import dj_database_url
+# import django_heroku
+# import dj_database_url
 from decouple import config,Csv
 import cloudinary
 import cloudinary.uploader
